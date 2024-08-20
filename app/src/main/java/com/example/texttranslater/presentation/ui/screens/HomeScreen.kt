a34314580ca903dccd7b09dac02633066bd68b8f
ZohaibKhanDev <mzkhan9610@gmail.com>
2024-08-20 15:59:13 +0500

    Bug Fix

--- app/src/main/java/com/example/texttranslater/presentation/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/texttranslater/presentation/ui/screens/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.example.texttranslater.presentation.ui.screens
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,11 +15,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -28,6 +33,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -39,6 +45,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -51,6 +58,7 @@ import com.example.texttranslater.presentation.viewmodel.MainViewModel
 import org.koin.compose.koinInject
 
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
@@ -61,12 +69,10 @@ fun HomeScreen() {
 
     var sourceLanguage by remember { mutableStateOf("English") }
     var targetLanguage by remember { mutableStateOf("Spanish") }
-
+    var isLoading by remember { mutableStateOf(false) }
 
     val languageMap = mapOf(
-        "English" to "en",
-        "Spanish" to "es",
-        "Chinese" to "zh"
+        "English" to "en", "Spanish" to "es", "Chinese" to "zh"
     )
 
     val availableLanguages = listOf(
@@ -77,15 +83,18 @@ fun HomeScreen() {
 
     when (translationState) {
         is ResultState.Error -> {
+            isLoading = false
             val error = (translationState as ResultState.Error).error
             Text(text = error.toString())
         }
 
         ResultState.Loading -> {
-            CircularProgressIndicator()
+            isLoading = true
         }
 
         is ResultState.Success -> {
+            isLoading = false
+
             val success = (translationState as ResultState.Success).success
             translateData = success
         }
@@ -103,45 +112,86 @@ fun HomeScreen() {
             Icon(imageVector = Icons.Default.Menu, contentDescription = "", tint = Color.White)
         }, colors = TopAppBarDefaults.topAppBarColors(containerColor = Color(0XFF003366)))
     }) {
-
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(top = it.calculateTopPadding()),
+                .padding(top = 100.dp),
             verticalArrangement = Arrangement.spacedBy(20.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(20.dp)
+            Card(
+                modifier = Modifier.padding(13.dp),
+                elevation = CardDefaults.cardElevation(2.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                )
             ) {
-                LanguageSelector(
-                    selectedLanguage = sourceLanguage,
-                    onLanguageSelected = { selected ->
-                        sourceLanguage = selected
-                    },
-                    availableLanguages = availableLanguages
-                )
+                Row(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(50.dp))
+                        .fillMaxWidth()
+                        .height(47.dp)
+                        .background(Color(0XFFf7f2f9)),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    LanguageSelector(
+                        selectedLanguage = sourceLanguage, onLanguageSelected = { selected ->
+                            sourceLanguage = selected
+                        }, availableLanguages = availableLanguages
+                    )
 
-                Icon(Icons.Default.ArrowForward, contentDescription = "Switch Language")
+                    Icon(Icons.Default.ArrowForward, contentDescription = "Switch Language")
 
-                LanguageSelector(
-                    selectedLanguage = targetLanguage,
-                    onLanguageSelected = { selected ->
-                        targetLanguage = selected
-                    },
-                    availableLanguages = availableLanguages
-                )
+                    LanguageSelector(
+                        selectedLanguage = targetLanguage, onLanguageSelected = { selected ->
+                            targetLanguage = selected
+                        }, availableLanguages = availableLanguages
+                    )
+                }
             }
 
-            OutlinedTextField(
-                value = inputText,
-                onValueChange = { inputText = it },
-                label = { Text("Enter text") },
+            Card(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 16.dp),
-            )
+                    .padding(15.dp)
+                    .height(216.dp),
+                elevation = CardDefaults.cardElevation(1.dp),
+                colors = CardDefaults.cardColors(containerColor = Color(0XFFf7f2f9))
+            ) {
+
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(5.dp),
+                        horizontalArrangement = Arrangement.SpaceAround,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(text = sourceLanguage)
+                    }
+
+                    TextField(
+                        value = inputText,
+                        onValueChange = { inputText = it },
+                        colors = TextFieldDefaults.colors(
+                            focusedIndicatorColor = Color.Transparent,
+                            focusedContainerColor = Color.Transparent,
+                            unfocusedContainerColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent
+                        ), modifier = Modifier
+                            .fillMaxWidth()
+                            .height(120.dp), shape = RoundedCornerShape(0.dp)
+                    )
+
+                }
+
+
+            }
 
             Button(
                 onClick = {
@@ -149,24 +199,32 @@ fun HomeScreen() {
                     val sourceCode = languageMap[sourceLanguage] ?: "en"
                     val targetCode = languageMap[targetLanguage] ?: "es"
                     viewModel.translateText(inputText, sourceCode, targetCode)
-                },
-                modifier = Modifier
+                }, modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp)
             ) {
                 Text(text = "Translate")
             }
-            Spacer(modifier = Modifier.height(16.dp))
-            translateData?.let {
-                Text(
-                    text = it,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Normal,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                )
+
+
+            if (isLoading) {
+                CircularProgressIndicator()
+            } else {
+                SelectionContainer {
+                    translateData?.let {
+                        Text(
+                            text = it,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Normal,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        )
+                    }
+                }
+
             }
+
         }
     }
 }
@@ -181,11 +239,9 @@ fun LanguageSelector(
     var expanded by remember { mutableStateOf(false) }
 
     Box {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
+        Row(verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(10.dp),
-            modifier = Modifier.clickable { expanded = true }
-        ) {
+            modifier = Modifier.clickable { expanded = true }) {
             Image(
                 painter = painterResource(id = availableLanguages.first { it.first == selectedLanguage }.second),
                 contentDescription = "$selectedLanguage Flag",
@@ -195,10 +251,7 @@ fun LanguageSelector(
             Icon(Icons.Default.ArrowDropDown, contentDescription = "Dropdown Icon")
         }
 
-        DropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { expanded = false }
-        ) {
+        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
             availableLanguages.forEach { language ->
                 DropdownMenuItem(text = {
                     Image(
